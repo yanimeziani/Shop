84ab47e66e49424030202e89c768d53294e7af98
Yani Meziani <yani.meziani@protonmail.com>
2023-08-21 14:47:08 -0400

    Fuck les toast

--- includes/header.php
+++ includes/header.php
@@ -63,19 +63,3 @@
     </div>
 
 </nav>
-<?php
-if (isset($_GET["message"])) {
-?>
-    <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-        <div class="toast-header">
-            <strong class="me-auto">Message</strong>
-            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-        </div>
-        <div class="toast-body">
-            <?php echo $_GET["message"]; ?>
-        </div>
-    </div>
-<?php
-}
-
-?>
