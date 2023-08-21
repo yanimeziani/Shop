befb219bdbae70c2d45938f6874ab5a501d8dcb3
Yani Meziani <yani.meziani@protonmail.com>
2023-08-21 12:55:40 -0400

    Wow

---
+++ includes/user.class.php
@@ -0,0 +1,81 @@
+<?php
+
+class User
+{
+    private $id;
+    private $email;
+    private $password;
+    private $first_name;
+    private $last_name;
+    private $shipping_address;
+
+    public function __construct($id, $email, $password, $first_name, $last_name, $shipping_address)
+    {
+        $this->id = $id;
+        $this->email = $email;
+        $this->password = $password;
+        $this->first_name = $first_name;
+        $this->last_name = $last_name;
+        $this->shipping_address = $shipping_address;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function getFirstName()
+    {
+        return $this->first_name;
+    }
+
+    public function getLastName()
+    {
+        return $this->last_name;
+    }
+
+    public function getShippingAddress()
+    {
+        return $this->shipping_address;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    public function setFirstName($first_name)
+    {
+        $this->first_name = $first_name;
+    }
+
+    public function setLastName($last_name)
+    {
+        $this->last_name = $last_name;
+    }
+
+    public function setShippingAddress($shipping_address)
+    {
+        $this->shipping_address = $shipping_address;
+    }
+
+    public function getFullName()
+    {
+        return $this->first_name . ' ' . $this->last_name;
+    }
+}
