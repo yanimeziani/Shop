ffc0f373fbfd3ef4745a47380e284acbd3a25d1a
Yani Meziani <yani.meziani@protonmail.com>
2023-08-21 14:40:45 -0400

    adawdawdaw

--- disconnect.php
+++ disconnect.php
@@ -2,7 +2,7 @@
 session_start();
 if (isset($_SESSION["email"])) {
     session_destroy();
-    header("Location: index.php");
+    header("Location: index.php?message=Déconnection réussie.");
 } else {
     header("Location: index.php");
 }
